2ce6e5d5c1f6078c4e7798bc75d60bf973100fe8
Raihan Mahamud <emonrait@gmail.com>
2021-10-21 11:48:28 +0600

    first commit

---
+++ app/src/main/java/com/example/tabsjetpackcompose/MainActivity.kt
@@ -0,0 +1,139 @@
+package com.example.tabsjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.*
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
+    @ExperimentalPagerApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@ExperimentalPagerApi
+@Composable
+fun MainScreen() {
+    val tabs = listOf(TabItem.Music, TabItem.Movies, TabItem.Books)
+    val pagerState = rememberPagerState()
+    Scaffold(
+        topBar = { TopBar() },
+    ) {
+        Column {
+            Tabs(tabs = tabs, pagerState = pagerState)
+            TabsContent(tabs = tabs, pagerState = pagerState)
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@ExperimentalPagerApi
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}
+
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = { Text(text = stringResource(R.string.app_name), fontSize = 18.sp) },
+        backgroundColor = colorResource(id = R.color.design_default_color_primary),
+        contentColor = Color.White
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    TopBar()
+}
+
+
+@ExperimentalMaterialApi
+@ExperimentalPagerApi
+@Composable
+fun Tabs(tabs: List<TabItem>, pagerState: PagerState) {
+    val scope = rememberCoroutineScope()
+    // OR ScrollableTabRow()
+    TabRow(
+        // Our selected tab is our current page
+        selectedTabIndex = pagerState.currentPage,
+        // Override the indicator, using the provided pagerTabIndicatorOffset modifier
+        backgroundColor = colorResource(id = R.color.design_default_color_primary),
+        contentColor = Color.White,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+            )
+        }) {
+        // Add tabs for all of our pages
+        tabs.forEachIndexed { index, tab ->
+            // OR Tab()
+            LeadingIconTab(
+                icon = { Icon(painter = painterResource(id = tab.icon), contentDescription = "") },
+                text = { Text(tab.title) },
+                selected = pagerState.currentPage == index,
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                },
+            )
+        }
+    }
+}
+
+@ExperimentalPagerApi
+@ExperimentalMaterialApi
+@Preview(showBackground = true)
+@Composable
+fun TabsPreview() {
+    val tabs = listOf(
+        TabItem.Music,
+        TabItem.Movies,
+        TabItem.Books
+    )
+    val pagerState = rememberPagerState()
+    Tabs(tabs = tabs, pagerState = pagerState)
+}
+
+@ExperimentalPagerApi
+@Composable
+fun TabsContent(tabs: List<TabItem>, pagerState: PagerState) {
+    HorizontalPager(state = pagerState, count = tabs.size) { page ->
+        tabs[page].screen()
+    }
+}
+
+
+@ExperimentalPagerApi
+@Preview(showBackground = true)
+@Composable
+fun TabsContentPreview() {
+    val tabs = listOf(
+        TabItem.Music,
+        TabItem.Movies,
+        TabItem.Books
+    )
+    val pagerState = rememberPagerState()
+    TabsContent(tabs = tabs, pagerState = pagerState)
+}
